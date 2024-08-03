e3914ee724e315037c58ea43e32cf044a49d7765
Jan Pfeifer <pfeifer@gmail.com>
2024-08-03 12:29:59 +0200

    Mark batch normalization with FrozenAverages if loading the pre-trained weights.

--- models/inceptionv3/model.go
+++ models/inceptionv3/model.go
@@ -491,7 +491,9 @@ func (cfg *Config) conv2DWithBatchNorm(ctx *context.Context, x *Node, kernelFilt
 	// Batch Normalization:
 	ctxWithWeights = cfg.readNextBatchNormalization(ctx, g) // Create a new context scope and read weights from `.h5` file.
 	x = batchnorm.New(ctxWithWeights, x, cfg.channelsAxis).CurrentScope().
-		Scale(cfg.batchNormScale).Epsilon(cfg.batchNormEpsilon).Trainable(cfg.trainable).Done()
+		Scale(cfg.batchNormScale).Epsilon(cfg.batchNormEpsilon).Trainable(cfg.trainable).
+		FrozenAverages(cfg.baseDir != ""). // If we are loading the weights, we don't want the averages to move.
+		Done()
 
 	// Apply:
 	x = activations.Relu(x)
