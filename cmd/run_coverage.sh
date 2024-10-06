b62c515ba5dafea0a32955d56e917406d728cc2f
Jan Pfeifer <pfeifer@gmail.com>
2024-10-06 20:57:25 +0200

    Fixed packages selection for `run_coverage.sh`

--- cmd/run_coverage.sh
+++ cmd/run_coverage.sh
@@ -3,6 +3,6 @@
 # Simple script to generate the coverage for GoMLX.
 # You should run this from the root of the repository.
 
-PACKAGE_COVERAGE="./graph/...,./ml/...,./models/...,./types/...,./xla/..."
+PACKAGE_COVERAGE="./graph/...,./ml/...,./models/...,./types/...,./backends/..."
 go test -v -cover -coverprofile docs/coverage.out -coverpkg "${PACKAGE_COVERAGE}" ./...
 go tool cover -func docs/coverage.out -o docs/coverage.out
