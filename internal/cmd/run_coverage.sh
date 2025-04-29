2cba970f2316e05f3ccb3eb6de17692fa363df37
Jan Pfeifer <pfeifer@gmail.com>
2025-04-29 13:00:51 +0200

    Updated coverage.

--- internal/cmd/run_coverage.sh
+++ internal/cmd/run_coverage.sh
@@ -3,6 +3,6 @@
 # Simple script to generate the coverage for GoMLX.
 # You should run this from the root of the repository.
 
-PACKAGE_COVERAGE="./graph/...,./ml/...,./models/...,./types/...,./backends/..."
+PACKAGE_COVERAGE="./graph/...,./ml/...,./models/...,./types/...,./backends,./backends/xla/...,./backends/shapeinference/...,./backends/simplego/..."
 go test -cover -coverprofile docs/coverage.out -coverpkg "${PACKAGE_COVERAGE}" ./... -test.count=1
 go tool cover -func docs/coverage.out -o docs/coverage.out
