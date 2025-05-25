d454551fe8b03f67a5a5e1764ec113c0eea19431
Jan <janpfeifer@users.noreply.github.com>
2025-05-25 18:48:05 +0200

    Update dotgeneral.go
    Comment out debug message left over.

--- backends/simplego/dotgeneral.go
+++ backends/simplego/dotgeneral.go
@@ -74,9 +74,9 @@ func (b *Builder) DotGeneral(lhsOp backends.Op, lhsContractingAxes, lhsBatchAxes
 	resultingDims = append(resultingDims, rhsCrossDims...)
 	result, err := b.Reshape(dotGeneral, resultingDims...)
 
-	fmt.Printf("DotGeneral(*lhs*: %s, c:%v, b:%v; *rhs*:  %s, c:%v, b:%v) -> %s\n",
-		lhs.shape, lhsContractingAxes, lhsBatchAxes, rhs.shape, rhsContractingAxes, rhsBatchAxes,
-		result.(*Node).shape)
+	// fmt.Printf("DotGeneral(*lhs*: %s, c:%v, b:%v; *rhs*:  %s, c:%v, b:%v) -> %s\n",
+	//	lhs.shape, lhsContractingAxes, lhsBatchAxes, rhs.shape, rhsContractingAxes, rhsBatchAxes,
+	//	result.(*Node).shape)
 
 	if err != nil {
 		return nil, err
