ec4a02dc084a1858a2392ce628393e5968d6c459
Jan Pfeifer <pfeifer@gmail.com>
2025-09-24 07:26:19 +0200

    models: skeleton of Exec and NewExec.

--- ml/models/models.go
+++ ml/models/models.go
@@ -10,3 +10,30 @@
 //
 // Example:
 package models
+
+import "github.com/gomlx/gomlx/ml/models/builderif"
+
+// Exec is an executor of models.
+//
+// It holds the "model" object (passed in NewExec), and for each different shaped inputs of the Call() (or MustCall) method,
+// it calls model.Build to rebuild the computation graph for that particular combination of inputs, JIT-compiles it and then executes it.
+// If the combination of shapes has already been seen before, it will reuses the pre-compiled graph -- up to a certain cache size.
+type Exec struct {
+	model     any
+	builderFn builderif.BuilderFn
+}
+
+// NewExec creates a new Exec object using the model object passed.
+// It keeps a reference to the model object, and it will use it every time it needs to build a new computation graph.
+//
+// It returns an error if the model object does not have a valid Builder API.
+func NewExec(model any) (*Exec, error) {
+	builderFn, err := builderif.ConvertToBuilderFn(model)
+	if err != nil {
+		return nil, err
+	}
+	return &Exec{
+		model:     model,
+		builderFn: builderFn,
+	}, nil
+}
