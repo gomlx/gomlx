1d08693c5b48c30b6b9fc0d46e8c87cfc69ce669
Jan Pfeifer <pfeifer@gmail.com>
2025-10-06 09:10:48 +0200

    Added non-panic version of fsutils

--- pkg/support/fsutil/fsutil.go
+++ pkg/support/fsutil/fsutil.go
@@ -10,28 +10,48 @@ import (
 	"github.com/pkg/errors"
 )
 
-// MustFileExists returns true if the file or directory exists.
+// MustFileExists returns whether the file or directory exists.
 // It panics on file system errors.
 func MustFileExists(path string) bool {
+	exists, err := FileExists(path)
+	if err != nil {
+		panic(err)
+	}
+	return exists
+}
+
+// FileExists returns whether the file or directory exists or an error if something went wrong in the filesystem.
+func FileExists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
-		return true
+		return true, nil
 	}
 	if errors.Is(err, os.ErrNotExist) {
-		return false
+		return false, nil
 	}
-	panic(err)
+	return false, errors.Wrapf(err, "failed to FileExists(%q)", path)
 }
 
 // MustReplaceTildeInDir by the user's home directory. Returns dir if it doesn't start with "~".
 //
 // It may panic with an error if `dir` has an unknown user (e.g: `~unknown/...`)
 func MustReplaceTildeInDir(dir string) string {
+	dir, err := ReplaceTildeInDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	return dir
+}
+
+// ReplaceTildeInDir by the user's home directory. Returns dir if it doesn't start with "~".
+//
+// It returns an error if `dir` has an unknown user or some other filesystem error (e.g: `~unknown/...`)
+func ReplaceTildeInDir(dir string) (string, error) {
 	if len(dir) == 0 {
-		return dir
+		return dir, nil
 	}
 	if dir[0] != '~' {
-		return dir
+		return dir, nil
 	}
 	var userName string
 	if dir != "~" && !strings.HasPrefix(dir, "~/") {
@@ -50,8 +70,8 @@ func MustReplaceTildeInDir(dir string) string {
 		usr, err = user.Lookup(userName)
 	}
 	if err != nil {
-		panic(errors.Wrapf(err, "failed to lookup home directory for user in path %q", dir))
+		return "", errors.Wrapf(err, "failed to lookup home directory for user in path %q", dir)
 	}
 	homeDir := usr.HomeDir
-	return path.Join(homeDir, dir[1+len(userName):])
+	return path.Join(homeDir, dir[1+len(userName):]), nil
 }
