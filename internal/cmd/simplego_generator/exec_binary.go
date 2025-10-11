0028ba9a7d24878860c7eac262ad6fd88c74f9a3
Jan Pfeifer <pfeifer@gmail.com>
2025-10-11 15:49:46 +0200

    Fixed simplego_generator.

--- internal/cmd/simplego_generator/exec_binary.go
+++ internal/cmd/simplego_generator/exec_binary.go
@@ -34,8 +34,9 @@ package simplego
 
 import (
 	"math"
+
 	"github.com/gomlx/gomlx/backends"
-	"github.com/gomlx/gomlx/core/shapes"
+	"github.com/gomlx/gomlx/pkg/core/shapes"
 	"github.com/gomlx/gopjrt/dtypes"
 	"github.com/gomlx/gopjrt/dtypes/bfloat16"
 	"github.com/pkg/errors"
