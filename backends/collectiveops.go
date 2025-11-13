c5135af45d8e0a394f1e58cc0def53fb43cb17ee
Jan Pfeifer <pfeifer@gmail.com>
2025-11-13 08:19:41 +0100

    Implementing AllReduce in backends.

--- backends/collectiveops.go
+++ backends/collectiveops.go
@@ -2,9 +2,9 @@ package backends
 
 // CollectiveOps is an interface for collective operations, that is, operations executed across multiple devices.
 type CollectiveOps interface {
-	//// AllReduce is a multi-device operation that reduces over the build the AllReduce operation across the configured devices.
-	//AllReduce(b Builder, reduceOp ReduceOpType, input Op, axis int) (Op, error)
-	//
+	// AllReduce is a multi-device operation that reduces over the build the AllReduce operation across the configured devices.
+	AllReduce(inputs []*Op, reduceOp ReduceOpType) ([]*Op, error)
+
 	//// AllGather builds the AllGather operation.
 	//AllGather(b Builder, gatherAxis int, input Op) (Op, error)
 	//
