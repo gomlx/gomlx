2330ce4a735e98dacd65202c80bdd13a9aaed7ce
Jan Pfeifer <pfeifer@gmail.com>
2026-01-08 15:14:11 +0100

    Fixed Capabilities.Clone()

--- backends/capabilities.go
+++ backends/capabilities.go
@@ -27,6 +27,7 @@ type Capabilities struct {
 // Clone makes a deep copy of the Capabilities.
 func (c Capabilities) Clone() Capabilities {
 	var c2 Capabilities
+	c2 = c
 	c2.Operations = make(map[OpType]bool, len(c.Operations))
 	maps.Copy(c2.Operations, c.Operations)
 	c2.DTypes = make(map[dtypes.DType]bool, len(c.DTypes))
