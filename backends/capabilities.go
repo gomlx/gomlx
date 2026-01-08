1e3063d3e50354b5c2f58f9e9d5fa2cff300c7c5
Jan Pfeifer <pfeifer@gmail.com>
2026-01-08 09:30:40 +0100

    Added "Functions" to backends.Capabilities.

--- backends/capabilities.go
+++ backends/capabilities.go
@@ -2,15 +2,22 @@
 
 package backends
 
-import "maps"
+import (
+	"maps"
 
-import "github.com/gomlx/gomlx/pkg/core/dtypes"
+	"github.com/gomlx/gomlx/pkg/core/dtypes"
+)
 
 // Capabilities holds mappings of what is supported by a backend.
 type Capabilities struct {
 	// Operations supported by a backend.
 	// If not listed, it's assumed to be false, hence not supported.
 	Operations map[OpType]bool
+
+	// Functions indicates whether the backend supports functions (top-level functions or closures).
+	// Without functions, it's not possible to support Call() op or any other
+	// op that takes as input a closure (While, If, etc.)
+	Functions bool
 
 	// DTypes list the data types supported by a backend.
 	// If not listed, it's assumed to be false, hence not supported.
