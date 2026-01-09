4a4b3c5a2aa3f5f9629d9113c1eb24226a417419
Jan Pfeifer <pfeifer@gmail.com>
2026-01-09 08:33:13 +0100

    Updated generated code.

--- pkg/core/graph/gen_backend_ops.go
+++ pkg/core/graph/gen_backend_ops.go
@@ -32,6 +32,7 @@ const (
 	NodeTypeBitwiseOr
 	NodeTypeBitwiseXor
 	NodeTypeBroadcastInDim
+	NodeTypeCall
 	NodeTypeCeil
 	NodeTypeClamp
 	NodeTypeClz
@@ -60,6 +61,7 @@ const (
 	NodeTypeGreaterThan
 	NodeTypeGreaterThanTotalOrder
 	NodeTypeIdentity
+	NodeTypeIf
 	NodeTypeImag
 	NodeTypeIota
 	NodeTypeIsFinite
@@ -113,11 +115,13 @@ const (
 	NodeTypeSign
 	NodeTypeSin
 	NodeTypeSlice
+	NodeTypeSort
 	NodeTypeSqrt
 	NodeTypeSub
 	NodeTypeTanh
 	NodeTypeTranspose
 	NodeTypeWhere
+	NodeTypeWhile
 )
 
 // nodeInputsAbs holds the inputs used for the call to backends.Abs.
@@ -796,6 +800,53 @@ func backendBroadcastInDim(x *Node, outputShape shapes.Shape, broadcastAxes []in
 	}
 	g.registerNode(node)
 	return
+}
+
+// nodeInputsCall holds the inputs used for the call to backends.Call.
+type nodeInputsCall struct {
+	f      Function
+	inputs []*Node
+}
+
+// Type implements the interface NodeInputs.
+func (ni *nodeInputsCall) Type() NodeType {
+	return NodeTypeCall
+}
+
+// String implements the interface NodeInputs.
+func (ni *nodeInputsCall) String() string {
+	return fmt.Sprintf("%s(f=%v, inputs=[#%s])",
+		ni.Type(),
+		ni.f,
+		strings.Join(xslices.Map(ni.inputs, func(node *Node) string { return fmt.Sprintf("#%d", node.Id()) }), ", "),
+	)
+}
+
+// Call a function with the given inputs.
+//
+// The function f must be from the same builder.
+func Call(f Function, inputs ...*Node) []*Node {
+	inputNodes := []*Node{}
+	inputNodes = append(inputNodes, inputs...)
+	g := validateBuildingGraphFromInputs(inputNodes...)
+	inputs := &nodeInputsCall{
+		f:      f,
+		inputs: slices.Clone(inputs),
+	}
+	results, err := g.currentFunc.backendFunc.Call(inputs.f, xslices.Map(inputs, func(node *Node) backends.Value { return node.outputOps[0] })...)
+	if err != nil {
+		panic(err)
+	}
+	node := &Node{
+		outputOps: results,
+		outputShapes: xslices.Map(results,
+			func(op backends.Value) shapes.Shape { return mustNoError(g.builder.OpShape(op)) }),
+		graph:      g,
+		inputs:     inputs,
+		inputNodes: inputNodes,
+	}
+	g.registerNode(node)
+	return splitNode(node)
 }
 
 // nodeInputsCeil holds the inputs used for the call to backends.Ceil.
@@ -2076,6 +2127,61 @@ func Identity(x *Node) (
 	}
 	g.registerNode(node)
 	return
+}
+
+// nodeInputsIf holds the inputs used for the call to backends.If.
+type nodeInputsIf struct {
+	pred        *Node
+	trueBranch  Function
+	falseBranch Function
+}
+
+// Type implements the interface NodeInputs.
+func (ni *nodeInputsIf) Type() NodeType {
+	return NodeTypeIf
+}
+
+// String implements the interface NodeInputs.
+func (ni *nodeInputsIf) String() string {
+	return fmt.Sprintf("%s(pred=[#%d], trueBranch=%v, falseBranch=%v)",
+		ni.Type(),
+		ni.pred.Id(),
+		ni.trueBranch,
+		ni.falseBranch,
+	)
+}
+
+// If executes one of two branches based on a boolean predicate.
+//
+// The pred must be a scalar boolean value.
+//
+// The trueBranch and falseBranch are closures that take no parameters (they can capture
+// values from the parent scope) and return N values each. Both branches must return
+// the same number of outputs with matching shapes.
+//
+// Returns the outputs of the executed branch.
+func If(pred *Node, trueBranch Function, falseBranch Function) []*Node {
+	inputNodes := []*Node{pred}
+	g := validateBuildingGraphFromInputs(inputNodes...)
+	inputs := &nodeInputsIf{
+		pred:        pred,
+		trueBranch:  trueBranch,
+		falseBranch: falseBranch,
+	}
+	results, err := g.currentFunc.backendFunc.If(pred.outputOps[0], inputs.trueBranch, inputs.falseBranch)
+	if err != nil {
+		panic(err)
+	}
+	node := &Node{
+		outputOps: results,
+		outputShapes: xslices.Map(results,
+			func(op backends.Value) shapes.Shape { return mustNoError(g.builder.OpShape(op)) }),
+		graph:      g,
+		inputs:     inputs,
+		inputNodes: inputNodes,
+	}
+	g.registerNode(node)
+	return splitNode(node)
 }
 
 // nodeInputsImag holds the inputs used for the call to backends.Imag.
@@ -4452,6 +4558,70 @@ func backendSlice(x *Node, starts []int, limits []int, strides []int) (
 	return
 }
 
+// nodeInputsSort holds the inputs used for the call to backends.Sort.
+type nodeInputsSort struct {
+	comparator Function
+	axis       int
+	isStable   bool
+	inputs     []*Node
+}
+
+// Type implements the interface NodeInputs.
+func (ni *nodeInputsSort) Type() NodeType {
+	return NodeTypeSort
+}
+
+// String implements the interface NodeInputs.
+func (ni *nodeInputsSort) String() string {
+	return fmt.Sprintf("%s(comparator=%v, axis=%v, isStable=%v, inputs=[#%s])",
+		ni.Type(),
+		ni.comparator,
+		ni.axis,
+		ni.isStable,
+		strings.Join(xslices.Map(ni.inputs, func(node *Node) string { return fmt.Sprintf("#%d", node.Id()) }), ", "),
+	)
+}
+
+// Sort sorts one or more tensors along the specified axis using a comparator closure.
+//
+// The comparator is a closure that takes 2*N scalar inputs (where N is the number of tensors)
+// and returns a single boolean. For each pair of positions being compared, it receives
+// (lhs_0, lhs_1, ..., lhs_N-1, rhs_0, rhs_1, ..., rhs_N-1) where lhs_i and rhs_i are scalars
+// from tensor i at the two positions being compared.
+//
+// The comparator should return true if lhs should come before rhs in the sorted order.
+// For a standard ascending sort on a single tensor, the comparator returns lhs < rhs.
+//
+// All input tensors must have the same shape. The axis must be valid for the input shape.
+// If isStable is true, the sort maintains the relative order of equal elements.
+//
+// Returns the sorted tensors in the same order as inputs.
+func Sort(comparator Function, axis int, isStable bool, inputs ...*Node) []*Node {
+	inputNodes := []*Node{}
+	inputNodes = append(inputNodes, inputs...)
+	g := validateBuildingGraphFromInputs(inputNodes...)
+	inputs := &nodeInputsSort{
+		comparator: comparator,
+		axis:       axis,
+		isStable:   isStable,
+		inputs:     slices.Clone(inputs),
+	}
+	results, err := g.currentFunc.backendFunc.Sort(inputs.comparator, inputs.axis, inputs.isStable, xslices.Map(inputs, func(node *Node) backends.Value { return node.outputOps[0] })...)
+	if err != nil {
+		panic(err)
+	}
+	node := &Node{
+		outputOps: results,
+		outputShapes: xslices.Map(results,
+			func(op backends.Value) shapes.Shape { return mustNoError(g.builder.OpShape(op)) }),
+		graph:      g,
+		inputs:     inputs,
+		inputNodes: inputNodes,
+	}
+	g.registerNode(node)
+	return splitNode(node)
+}
+
 // nodeInputsSqrt holds the inputs used for the call to backends.Sqrt.
 type nodeInputsSqrt struct {
 	x *Node
@@ -4668,4 +4838,63 @@ func backendWhere(condition *Node, onTrue *Node, onFalse *Node) (
 	}
 	g.registerNode(node)
 	return
+}
+
+// nodeInputsWhile holds the inputs used for the call to backends.While.
+type nodeInputsWhile struct {
+	cond         Function
+	body         Function
+	initialState []*Node
+}
+
+// Type implements the interface NodeInputs.
+func (ni *nodeInputsWhile) Type() NodeType {
+	return NodeTypeWhile
+}
+
+// String implements the interface NodeInputs.
+func (ni *nodeInputsWhile) String() string {
+	return fmt.Sprintf("%s(cond=%v, body=%v, initialState=[#%s])",
+		ni.Type(),
+		ni.cond,
+		ni.body,
+		strings.Join(xslices.Map(ni.initialState, func(node *Node) string { return fmt.Sprintf("#%d", node.Id()) }), ", "),
+	)
+}
+
+// While executes a loop while a condition is true.
+//
+// The condition closure (cond) takes N values (the current state) and returns a single
+// boolean scalar indicating whether to continue looping.
+//
+// The body closure takes N values (the current state) and returns N values (the new state).
+// The shapes of the outputs must match the shapes of the inputs.
+//
+// The initialState values are passed to both cond and body on the first iteration.
+// On subsequent iterations, the outputs of body become the new state.
+//
+// Returns the final state values when cond returns false.
+func While(cond Function, body Function, initialState ...*Node) []*Node {
+	inputNodes := []*Node{}
+	inputNodes = append(inputNodes, initialState...)
+	g := validateBuildingGraphFromInputs(inputNodes...)
+	inputs := &nodeInputsWhile{
+		cond:         cond,
+		body:         body,
+		initialState: slices.Clone(initialState),
+	}
+	results, err := g.currentFunc.backendFunc.While(inputs.cond, inputs.body, xslices.Map(initialState, func(node *Node) backends.Value { return node.outputOps[0] })...)
+	if err != nil {
+		panic(err)
+	}
+	node := &Node{
+		outputOps: results,
+		outputShapes: xslices.Map(results,
+			func(op backends.Value) shapes.Shape { return mustNoError(g.builder.OpShape(op)) }),
+		graph:      g,
+		inputs:     inputs,
+		inputNodes: inputNodes,
+	}
+	g.registerNode(node)
+	return splitNode(node)
 }
