b522576158dd1697e6af6a24eda90debef2d8d5c
Jan Pfeifer <pfeifer@gmail.com>
2026-04-21 08:17:44 +0200

    Added support.testutil. Moved BuildTestBackend and TestOfficialBackends to the new testutil.

--- pkg/core/graph/graphtest/graphtest.go
+++ pkg/core/graph/graphtest/graphtest.go
@@ -5,91 +5,35 @@ package graphtest
 
 import (
 	"fmt"
-	"os"
-	"slices"
-	"sync"
 	"testing"
 
 	"github.com/gomlx/compute"
 	"github.com/gomlx/compute/shapes"
 	"github.com/gomlx/compute/support/xslices"
-	"github.com/gomlx/go-xla/pkg/installer"
-	"github.com/gomlx/gomlx/backends/xla"
 	"github.com/gomlx/gomlx/pkg/core/graph"
 	"github.com/gomlx/gomlx/pkg/core/tensors"
+	"github.com/gomlx/gomlx/pkg/support/testutil"
 	"github.com/stretchr/testify/require"
-	"k8s.io/klog/v2"
 )
 
 // TestGraphFn should build its own inputs, and return both inputs and outputs
 type TestGraphFn func(g *graph.Graph) (inputs, outputs []*graph.Node)
 
-var (
-	backendOnce   sync.Once
-	cachedBackend compute.Backend
-)
-
 // BuildTestBackend and sets compute.DefaultConfig to "xla:cpu" -- it can be overwritten by GOMLX_BACKEND environment variable.
+//
+// Deprecated: use testutil.BuildTestBackend instead.
 func BuildTestBackend() compute.Backend {
-	compute.DefaultConfig = officialTestBackendNames[0]
-	backendOnce.Do(func() {
-		err := xla.AutoInstall()
-		if err != nil {
-			klog.Fatalf("Failed to auto-install XLA PJRT: %+v", err)
-		}
-		for i, backendName := range officialTestBackendNames {
-			backend, err := compute.NewWithConfig(backendName)
-			if err != nil {
-				if i == 0 {
-					klog.Fatalf("Failed to create backend %q: %+v", backendName, err)
-				}
-				klog.Errorf("Failed to create backend %q: %+v", backendName, err)
-				continue
-			}
-			officialTestBackends[backendName] = backend
-		}
-	})
-	return officialTestBackends[compute.DefaultConfig]
-}
-
-var (
-	officialTestBackendNames []string = []string{
-		"xla:cpu",
-		"go",
-	}
-	officialTestBackends = make(map[string]compute.Backend)
-)
-
-func init() {
-	if selectedBackendName := os.Getenv(compute.ConfigEnvVar); selectedBackendName != "" {
-		officialTestBackendNames = []string{selectedBackendName}
-		return
-	}
-
-	// Include CUDA PJRT test if available.
-	if installer.HasNvidiaGPU() {
-		officialTestBackendNames = append(officialTestBackendNames, "xla:cuda")
-	}
+	return testutil.BuildTestBackend()
 }
 
 // TestOfficialBackends iterates over list of backends and calls testFn for each of them.
 // If GOMLX_BACKEND environment variable is set, it will only iterate over the one set.
 // If GOMLX_BACKEND is not set, it will iterate over all official backends, except those in excludecompute.
 // (for tests known not to work on those backends)
+//
+// Deprecated: use testutil.TestOfficialBackends instead.
 func TestOfficialBackends(t *testing.T, testFn func(t *testing.T, backend compute.Backend), excludeBackends ...string) {
-	BuildTestBackend()
-	for backendName, backend := range officialTestBackends {
-		if slices.Contains(excludeBackends, backendName) {
-			continue
-		}
-		if backend == nil {
-			// This happens if the backend already failed to initialize, no need to report it more than once.
-			continue
-		}
-		t.Run(backendName, func(t *testing.T) {
-			testFn(t, backend)
-		})
-	}
+	testutil.TestOfficialBackends(t, testFn, excludeBackends...)
 }
 
 // RunTestGraphFn tests a graph building function graphFn by executing it and comparing
