2a66a9c0c7861285b315292cd81aeeac1f473523
Jan Pfeifer <pfeifer@gmail.com>
2026-04-27 08:42:02 +0200

    Include window/amd64 for XLA backends.

--- pkg/support/testutil/xla.go
+++ pkg/support/testutil/xla.go
@@ -1,6 +1,6 @@
 // Copyright 2023-2026 The GoMLX Authors. SPDX-License-Identifier: Apache-2.0
 
-//go:build ((linux && (amd64 || arm64)) || (darwin && arm64)) && !noxla
+//go:build ((linux && (amd64 || arm64)) || (darwin && arm64) || (windows && amd64)) && !noxla
 
 package testutil
 
